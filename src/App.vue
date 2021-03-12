7ab4b7942884e5c07bee30574085660003d628e6
Carly Perdue <carlyperdue@gmail.com>
2021-03-12 10:59:00 -0500

    added course schedule

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,82 @@
 <template>
   <div id="app">
-    <h2>VueJS Experiment by  {{ myInfo.name }}</h2>
+    <h2>VueJS Experiment by {{ myInfo.name }}</h2>
     <h2>Personal Info</h2>
-    Name: {{myInfo.name}}
-    Semester: {{myInfo.semester}} {{myInfo.year}}<br/>
+    Name: {{ myInfo.name }} Semester: {{ myInfo.semester }} {{ myInfo.year
+    }}<br />
     Courses I am taking
     <ol>
       <li v-for="(c, pos) in myInfo.courses" :key="pos">
-        {{c.name}} ({{c.credits}} credits)</li>
-    </ol>    
-
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!--<hello-world msg="Welcome to Your Vue.js + TypeScript App"/>-->
+        {{ c.name }} ({{ c.credits }} credits)
+      </li>
+    </ol>
+    <table>
+      <thead>
+        <th>Course</th>
+        <th>Credits</th>
+      </thead>
+      <tbody>
+        <tr v-for="(c, pos) in myInfo.courses" :key="pos">
+          <td>{{ c.name }}</td>
+          <td>{{ c.credits }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <CourseCard
+      v-for="(c, pos) in myInfo.courses"
+      :key="pos"
+      v-bind:name="c.name"
+      v-bind:num-credits="c.credits"
+      v-bind:meetings="c.meetings">
+    </CourseCard>
+    <!--<hello-world msg="Wellcome to Your Vue.js + TypeScript App"/>-->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import CourseCard from "./components/CourseCard.vue";
 
 @Component({
   components: {
     HelloWorld,
+    CourseCard,
   },
 })
 export default class App extends Vue {
-  readonly me = "Carly"
   readonly myInfo = {
     name: "Carly",
     semester: "Winter",
     year: 2021,
     courses: [
-      {name: "CIS 263 Data Structures and Algorithms", credits: 3},
-      {name: "CIS 371 Web Application Programming", credits: 3},
-      {name: "CIS 290 Professional Responsibilities and Practices", credits: 3},
-      {name: "MTH 325 Discrete Structures: Computer Science 2", credits: 3},
-      {name: "MTH 312 Cryptography and Privacy", credits: 3},
+      {
+        name: "CIS 263 Data Structures and Algorithms",
+        credits: 3,
+        meetings: ["Mon 8-9a", "Fri 8-9a"],
+      },
+      { 
+        name: "CIS 371 Web Application Programming", 
+        credits: 3,
+        meetings: ["Mon 2-3p", "Wed 2-3p", "Fri 2-3p"],
+      },
+
+      {
+        name: "CIS 290 Professional Responsibilities and Practices",
+        credits: 3,
+        meetings: ["Thu 10-11:15p"]
+      },
+      { 
+        name: "MTH 325 Discrete Structures: Computer Science 2", 
+        credits: 3,
+        meetings: ["Mon 10-11a"]
+        },
+      { name: "MTH 312 Cryptography and Privacy", 
+        credits: 3,
+        meetings: ["Mon 12-1p","Wed 12-1p", "Fri 12-1p"]
+       },
     ],
-  }
+  };
 }
 </script>
 
@@ -46,8 +85,18 @@ export default class App extends Vue {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+table,
+tr,
+td,
+th {
+  border: 1px solid black;
+}
+
+td:nth-of-type(2) {
+  text-align: center;
 }
 </style>
---
+++ src/components/CourseCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="crs">
+      <h3>Course: {{name}}</h3>
+      <h3>Credits: {{numCredits}}</h3>
+      <h3>Meetings: {{meetings}}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class CourseCard extends Vue {
+    @Prop() readonly name!: string;
+    @Prop() readonly numCredits!: number;
+    @Prop() readonly meetings!: string;
+  }
+</script>
+
+<style scoped>
+#crs {
+  display: inline-block;
+  border: 2px solid hsl(30, 90%, 50%);
+  border-radius: 0.5em;
+  padding:1em;
+  margin: 0.6em;
+  box-shadow: 3px 5px 6px 7px hsla(30, 55%, 50%, 0.4);
+}
+</style>
